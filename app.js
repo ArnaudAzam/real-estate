b4ca6f755e295a781eb5fa384087059ff7b0827f
Arnaud Azam <arnaud.a94@hotmail.fr>
2017-02-01 23:29:51 +0100

    le fichier a bien été commit

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ app.use( '/assets', express.static( 'assets' ) );
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
+    callLeboncoin();
     res.render( 'home', {
         message: 'The Home Page!'
     });
+
 });
 
 
@@ -25,3 +27,28 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+function callLeboncoin() {
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/904 912363.htm?ca=12_s';
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+            const $ = cheerio.load( html )
+
+            const lbcDataArray = $( 'section.properties span.value' )
+
+            let lbcData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                room: $( lbcDataArray.get( 3 ) ).text(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 ),
+
+            }
+            console.log( lbcData )
+        }
+        else {
+            console.log( error )
+        }
+    })
+}
